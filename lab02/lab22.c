0f4d9672ba9cc83db16a9f491a7bad1fe8b0112c
zbtka <144006033+zbtka@users.noreply.github.com>
2023-11-06 14:07:53 +0500

    lab22.c

--- lab02/lab22.c
+++ lab02/lab22.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    double x, y;
+    double eps;
+    double h;
+    scanf("%lf", &h);
+    eps = h / 2.0;
+
+    printf("x\t\t\ty\n");
+
+    for (x = 0.0; x <= 2.0 + eps; x += h) {
+        if (x >= 0.0 && x <= 1.0 + eps) {
+            y = sqrt(x + 1) - sqrt(x) - 0.5;
+        } else if (x > 1.0 + eps && x <= 2.0 + eps) {
+            y = exp(-x - 1 / x);
+        } else {
+            y = 0.0;
+        }
+
+        printf("%.6f\t%.6f\n", x, y);
+    }
+
+    return 0;
+}
