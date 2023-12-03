cefcd1bf058b8ba4b557d2913913bfe135f40bc3
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-03 16:23:15 +0500

    00_distance.py

--- lab04/00_distance.py
+++ lab04/00_distance.py
@@ -18,9 +18,9 @@ distances = {}
 MoscowLondon = ((sites['Moscow'][0] - sites['London'][0]) **2 + (sites['Moscow'][1] - sites['London'][1]) **2 ) ** 0.5
 MoscowParis =  ((sites['Moscow'][0] - sites['Paris'][0]) **2 + (sites['Moscow'][1] - sites['Paris'][1]) **2 ) ** 0.5
 LondonParis = ((sites['London'][0] - sites['Paris'][0]) **2 + (sites['London'][1] - sites['Paris'][1]) **2 ) ** 0.5
-distances['Moscow_and_London'] = round(MoscowLondon, 1)
-distances['Moscow_and_Paris'] = round(MoscowParis, 1)
-distances['London_and_Paris'] = round(LondonParis, 1)
+distances['Moscow-London'] = round(MoscowLondon, 1)
+distances['Moscow-Paris'] = round(MoscowParis, 1)
+distances['London-Paris'] = round(LondonParis, 1)
 print(distances)
 
 
