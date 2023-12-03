5115954ce43f46b8d3094781065dd648e0850c6f
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-03 16:06:28 +0500

    08_garden.py

--- lab04/08_garden.py
+++ lab04/08_garden.py
@@ -11,15 +11,17 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # garden_set =
 # meadow_set =
 # TODO здесь ваш код
-
+garden_set = set(garden)
+meadow_set = set(meadow)
 # выведите на консоль все виды цветов
 # TODO здесь ваш код
-
+print(garden_set.union(meadow_set))
 # выведите на консоль те, которые растут и там и там
 # TODO здесь ваш код
-
+print(garden_set & meadow_set)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
 # TODO здесь ваш код
-
+print(garden_set.difference(meadow_set))
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 # TODO здесь ваш код
+print(meadow_set.difference(garden_set))
