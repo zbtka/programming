6745948cfbc81198a0fe8e1047dac3f46b738f1d
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-03 15:40:03 +0500

    00_distance.py

--- lab04/00_distance.py
+++ lab04/00_distance.py
@@ -13,11 +13,15 @@ sites = {
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 distances = {}
-
 # TODO здесь заполнение словаря
 
+MoscowLondon = ((sites['Moscow'][0] - sites['London'][0]) **2 + (sites['Moscow'][1] - sites['London'][1]) **2 ) ** 0.5
+MoscowParis =  ((sites['Moscow'][0] - sites['Paris'][0]) **2 + (sites['Moscow'][1] - sites['Paris'][1]) **2 ) ** 0.5
+LondonParis = ((sites['London'][0] - sites['Paris'][0]) **2 + (sites['London'][1] - sites['Paris'][1]) **2 ) ** 0.5
+distances['Moscow_and_London'] = round(MoscowLondon, 1)
+distances['Moscow_and_Paris'] = round(MoscowParis, 1)
+distances['London_and_Paris'] = round(LondonParis, 1)
 print(distances)
-
 
 
 
