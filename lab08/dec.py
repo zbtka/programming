761090b9ebba12536ce6c7b2ee2429047dc36154
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-17 17:20:22 +0500

    dec.py

--- lab08/dec.py
+++ lab08/dec.py
@@ -1,1 +1,15 @@
+import functools
 
+def limit_calls(max_calls):
+    def decorator(func):
+        calls = 0
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal calls
+            if calls < max_calls:
+                calls += 1
+                return func(*args, **kwargs)
+            else:
+                return "Максимальное количество вызовов превышено"
+        return wrapper
+    return decorator
