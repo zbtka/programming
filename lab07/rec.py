65d6700720841764b83d3075e055e655662b18d9
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-17 16:54:18 +0500

    rec.py

--- lab07/rec.py
+++ lab07/rec.py
@@ -1,1 +1,12 @@
+def to_str_recursive(lst):
+    if isinstance(lst, list):
+        if len(lst) == 0:
+            return "None"
+        else:
+            return str(lst[0]) + " -> " + to_str_recursive(lst[1:])
+    else:
+        return str(lst) + " -> None"
 
+nested_list = [1, [2, [3, [4, [5]]]]]
+string_recursive = to_str_recursive(nested_list)
+print(string_recursive)
