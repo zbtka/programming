f17e69a4812491ee77df992602b2730becb8e86a
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-17 17:30:20 +0500

    zam.py

--- lab08/zam.py
+++ lab08/zam.py
@@ -1,1 +1,13 @@
+def unique_values_closure():
+    unique_values = set()
 
+    def inner(*args):
+        nonlocal unique_values
+        unique_values.update(args)
+        return list(unique_values)
+
+    return inner
+#пример
+unique_values = unique_values_closure()
+print(unique_values(1, 2, 3, 2, 4, 5, 3, 6))
+print(unique_values(5, 6, 7, 8, 1, 2, 3, 4))
