2bdeea16a569407010843e6c0fc58c4060660c63
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-19 15:18:38 +0500

    1n_rec.py

--- lab07/1n_rec.py
+++ lab07/1n_rec.py
@@ -1,10 +1,17 @@
 def to_str(lst):
-    if lst is None:
-        return "None"
-    if isinstance(lst, list):
-        if len(lst) == 0:
-            return "None"
-        return " -> ".join(map(to_str, lst)) + " -> None"
-    return str(lst)
-result = to_str([1, [2, [3, [4, [5]]]]])
-print(result)
+    result = []
+    stack = [lst]
+    while stack:
+        current = stack.pop()
+        if isinstance(current, list):
+            if current:
+                stack.extend(reversed(current[1:]))
+                stack.append(current[0])
+            else:
+                result.append("None")  
+        else:
+            result.append(str(current))
+    result.append("None") 
+    return " -> ".join(result)
+nested_list = [1, [2, [3, [4, [5]]]]]
+print(to_str(nested_list))
