f9dfa350be10c4553cc929683a20c0a243c8543b
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-19 19:46:58 +0500

    Update lab3.c

--- lab03/lab3.c
+++ lab03/lab3.c
@@ -17,27 +17,43 @@ int main() {
     printf("array A:\n");
     for (int i = 0; i < sizeA; i++) {
         arrA[i] = rand() % 10;
-        printf("%d ", arrA[i]);
+        printf("%5d", arrA[i]);
     }
     printf("\n");
 
     printf("array B:\n");
     for (int i = 0; i < sizeB; i++) {
         arrB[i] = rand() % 10; 
-        printf("%d ", arrB[i]);
+        printf("%5d", arrB[i]);
     }
     printf("\n");
-
+    int s = 0;
     printf("\narray C:\n");
     for (int i = 0; i < sizeA; i++) {
         for (int j = 0; j < sizeB; j++) {
             if (arrA[i] == arrB[j]) {
+                s++;
                 printf("%d ", arrA[i]);
-                break;
-            }
+                }
+                }
+    }
+    printf("s = %d\n ", s);
+    int arrC[s];
+    int k = 0;
+    printf("\narray C:\n");
+    for (int i = 0; i < sizeA; i++) {
+        for (int j = 0; j < sizeB; j++) {
+            if (arrA[i] == arrB[j]) {
+                arrC[k++] = arrA[i];
+                printf("%5d", arrC[k-1]);
+                }   
         }
     }
-    printf("\n");
 
+    printf("k = %d\n ", k);
+    for (int i = 0; i < s; i++) {
+        printf("%5d", arrC[i]);  
+    }
+    printf("\n");
     return 0;
 }
