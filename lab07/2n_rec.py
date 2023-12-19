94def6f377576309ef8a07f29e6da0b822fe4ff9
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-19 15:35:18 +0500

    2n_rec.py

--- lab07/2n_rec.py
+++ lab07/2n_rec.py
@@ -1,19 +1,12 @@
-def calculate_sequence(n):
-    if n < 0:
-        return None
-    elif n == 0 or n == 1:
+def calculate_a_iterative(i, a):
+    if i < 2:
         return 1
-    
-    a_i_minus_2 = 1
-    a_i_minus_1 = 1
-    a_i = 0
-
-    for i in range(2, n + 1):
-        a_i = a_i_minus_2 + a_i_minus_1 / (a_i_minus_1 ** (a_i_minus_1 - 1))
-        a_i_minus_2 = a_i_minus_1
-        a_i_minus_1 = a_i
-    
-    return a_i
-
-result = calculate_sequence(10)
-print(result)
+    a_values = [1, 1] 
+    for n in range(2, i+1):
+        a_n = a_values[n-2] + (a_values[n-1] / (a ** (a-1)))
+        a_values.append(a_n)
+    return a_values[i]
+i_value = 5
+a_value = 2
+result = calculate_a_iterative(i_value, a_value)
+print(f"i={i_value} и a={a_value} = {result}")
