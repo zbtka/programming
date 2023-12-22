7abf386a85e46ab04b915ba9ca5ebe022a77fb4a
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-23 01:26:42 +0500

    rec.py

--- lab10/moduls/rec.py
+++ lab10/moduls/rec.py
@@ -1,20 +1,7 @@
-def calculate_recursive(expression):
-    if isinstance(expression, list):
-        operand1 = calculate_recursive(expression[1])
-        operand2 = calculate_recursive(expression[2])
-        operator = expression[0]
-        if operator == '+':
-            return operand1 + operand2
-        elif operator == '-':
-            return operand1 - operand2
-        elif operator == '*':
-            return operand1 * operand2
-        elif operator == '/':
-            return operand1 / operand2
-    else:
-        return expression
-
-# пример
-expr = ['+', 5, ['*', 3, 2]]  # результат: 11
-result = calculate_recursive(expr)
-print(result)
+def calculate_a_iterative(n):
+    a = [1, 1]
+    for i in range(2, n+1):
+        a.append(a[i-2] + a[i-1] / (2**(i-1)))
+    return a[n]
+result_iterative = calculate_a_iterative(5)
+print(result_iterative)
