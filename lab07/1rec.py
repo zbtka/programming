9178501706147d657a090af7c053a4f11b833750
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-23 17:22:15 +0500

    Update 1rec.py

--- lab07/1rec.py
+++ lab07/1rec.py
@@ -1,11 +1,21 @@
-def to_str(input_list, is_outer=True):
-    if isinstance(input_list, list):
+def to_str(input_list, is_outer=True,):
+    if not input_list:
+        return "None"
+    elif isinstance(input_list, list):
         result = [to_str(item, False) for item in input_list]
         if is_outer:
             return ' -> '.join(result) + ' -> None' 
         else:
             return ' -> '.join(result)
     else:
-        return str(input_list) if input_list is not None else 'None'
+        return str(input_list) if input_list is not "None" else 'None'
+
+
 result = to_str([1, [2, [3, [4, [5]]]]])
+result2 = to_str([])
+result3 = to_str([1, [2]])
+
+
 print(result)
+print(result2)
+print(result3)
