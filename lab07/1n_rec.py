69b79a4b660177f509de8ab52c84f1dcd8f1a828
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-23 17:21:56 +0500

    Update 1n_rec.py

--- lab07/1n_rec.py
+++ lab07/1n_rec.py
@@ -1,4 +1,7 @@
 def to_str(lst):
+    if not lst: 
+        return "None" 
+
     result = []
     stack = [lst]
     while stack:
@@ -8,10 +11,14 @@ def to_str(lst):
                 stack.extend(reversed(current[1:]))
                 stack.append(current[0])
             else:
-                result.append("None")  
+                result.append("None")
         else:
             result.append(str(current))
-    result.append("None") 
+    result.append("None")
     return " -> ".join(result)
+
 nested_list = [1, [2, [3, [4, [5]]]]]
+
 print(to_str(nested_list))
+print(to_str([]))
+print(to_str([1, [2]]))
