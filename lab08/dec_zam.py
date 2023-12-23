03400c4cb42f8f6e1dde7b059bb1a08dc02f983f
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-23 17:32:44 +0500

    dec_zam.py

--- lab08/dec_zam.py
+++ lab08/dec_zam.py
@@ -14,13 +14,16 @@ def limit_calls(max_calls):
         return wrapper
     return decorator
     
-@limit_calls(4)
-def unique_values_closure(*args):
+def unique_values_closure():
     unique_values = set()
-    unique_values.update(args)
-    return list(unique_values)
-    
-print(unique_values_closure(1, 2, 3, 2, 4, 5, 3, 6))
-print(unique_values_closure(5, 6, 7, 8, 1, 2, 3, 4))
-print(unique_values_closure(9, 1, 2, 3, 4, 5, 6, 3))
-print(unique_values_closure(2, 0, 3, 0, 2, 4, 8, 5))
+    @limit_calls(1)
+
+    def inner(*args):
+        nonlocal unique_values
+        unique_values.update(args)
+        return list(unique_values)
+
+    return inner
+unique_values = unique_values_closure()
+print(unique_values(1, 2, 3, 2, 4, 5, 3, 6))
+print(unique_values(5, 6, 7, 8, 1, 2, 3, 4))
