9c24a8b672d644fbfcb6ceb1e225b8ae1b09d83c
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-29 01:07:36 +0500

    typer.py

--- lab10/typer.py
+++ lab10/typer.py
@@ -1,17 +1,11 @@
 import typer
+
 from moduls import dec_zam, gen, rec
+
 app = typer.Typer()
 
-@app.command()
-def gen1(seq: str):
-    args = eval(seq)
-    for combo in gen.generate_combinations(*args):
-        print(combo)
 
-@app.command()
-def rec(a: str):
-    print(a)
+@app.comand()
+def gen(name: gen, seq: str = typer.Option()):
+    print(def)
 
-
-if __name__ == "__main__":
-    app()
