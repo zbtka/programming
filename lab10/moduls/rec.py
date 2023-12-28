a71ccb8f73e84ee6c3cf0c34747b480dcff9ce77
zbtka <144006033+zbtka@users.noreply.github.com>
2023-12-29 01:02:46 +0500

    Delete lab10/moduls/rec.py

--- lab10/moduls/rec.py
+++
@@ -1,7 +0,0 @@
-def calculate_a_iterative(n):
-    a = [1, 1]
-    for i in range(2, n+1):
-        a.append(a[i-2] + a[i-1] / (2**(i-1)))
-    return a[n]
-result_iterative = calculate_a_iterative(5)
-print(result_iterative)
